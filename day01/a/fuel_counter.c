2ca29454f662cf91605ea2cac8e659bc416c801a
Luke Kosewski <lukasz.kosewski@harley-davidson.com>
2019-12-03 15:39:55 -0800

    Added day 1 part 1 solution

---
+++ day01/a/fuel_counter.c
@@ -0,0 +1,26 @@
+#include "aocinput.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char linebuf[2048];
+	input_t *pfinfo;
+
+	pfinfo = setup_input("input.txt");
+
+	int line_len;
+	int total = 0;
+	while (line_len = get_a_line(pfinfo, linebuf)) {
+		int n = atoi(linebuf);
+		int a = n / 3 - 2;
+		total += a;
+	}
+
+	printf("%d\n", total);
+
+	teardown_input(pfinfo);
+
+	return 0;
+}
