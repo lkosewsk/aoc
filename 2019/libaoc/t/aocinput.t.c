fcadf21621a258e504bad9a5f13e6cda98c9eac2
Luke Kosewski <lukasz.kosewski@harley-davidson.com>
2019-12-04 09:43:43 -0800

    aocinput.t.cc: Test degenerate case of "read the last line"
    Even if it has no lf on the end.

--- 2019/libaoc/t/aocinput.t.c
+++ 2019/libaoc/t/aocinput.t.c
@@ -1,6 +1,8 @@
 #include "wvtest.h"
 #include "aocinput.h"
 
+#include "string.h"
+
 WVTEST_MAIN("basic functionality")
 {
 	char linebuf[512];
@@ -8,6 +10,19 @@ WVTEST_MAIN("basic functionality")
 
 	int l = aocinput_getline(inp, linebuf);
 	WVPASSEQ(l, 2);
+
+	aocinput_teardown(inp);
+}
+
+WVTEST_MAIN("ensure works even with no final char")
+{
+	char linebuf[512];
+	input_t *inp = aocinput_setup("t/nolf.txt");
+
+	int l = aocinput_getline(inp, linebuf);
+	WVPASSEQ(l, strlen(linebuf));
+	WVFAILEQ(linebuf[l - 1], '\n');
+	WVPASSEQSTR(linebuf, "noeol");
 
 	aocinput_teardown(inp);
 }
