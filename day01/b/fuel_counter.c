246bc4b20fd25578914a78c4f94379bce0247061
Luke Kosewski <lukasz.kosewski@harley-davidson.com>
2019-12-03 16:04:36 -0800

    Day 1 part 2.  I don't like how these Makefiles are done but speed...

--- day01/b/fuel_counter.c
+++ day01/b/fuel_counter.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int calculate_fuel(int weight)
+{
+	int a = weight / 3 - 2;
+	if (a <= 0)
+		return 0;
+	return a + calculate_fuel(a);
+}
+
 int main()
 {
 	char linebuf[2048];
@@ -13,8 +21,7 @@ int main()
 	int line_len;
 	int total = 0;
 	while (line_len = get_a_line(pfinfo, linebuf)) {
-		int n = atoi(linebuf);
-		int a = n / 3 - 2;
+		int a = calculate_fuel(atoi(linebuf));
 		total += a;
 	}
 
